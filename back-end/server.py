8a1fa4ba1356c7a5747859a43db92c37362dd411
CodeFromKerala <mjnair52@gmail.com>
2025-09-08 21:32:36 +0530

    Added External File Rendering

--- back-end/server.py
+++ back-end/server.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return "<h1>Hello, World!</h1>"
+
+@app.route('/homepage')
+def hello_world_homepage():
+	return "<h1> No </h1>"
+
+@app.route('/actualfile')
+def actual_file():
+	return render_template("index.html")
 
 app.run(host='0.0.0.0', port='8080', debug=True)
