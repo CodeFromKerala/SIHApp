4133b0aa0c92fa64a5559c35dd47ee5f82fd1c88
CodeFromKerala <mjnair52@gmail.com>
2025-09-09 15:24:51 +0530

    Changed server.py file to one extension

--- back-end/server.py
+++ back-end/server.py
@@ -3,14 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return "<h1>Hello, World!</h1>"
-
-@app.route('/homepage')
-def hello_world_homepage():
-	return "<h1> No </h1>"
-
-@app.route('/actualfile')
 def actual_file():
 	return render_template("index.html")
 
