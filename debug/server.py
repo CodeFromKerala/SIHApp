9c5effcc0a27769a83de86c14d81476c21ddec67
CodeFromKerala <mjnair52@gmail.com>
2025-09-16 15:25:06 +0530

    Added Login/Register placeholder pages

--- debug/server.py
+++ debug/server.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def actual_file():
-	return render_template("./home-page/index.html", version='4')
+@app.route('/', methods=["GET", "POST"])
+def home_page():
+	if request.method == 'POST':
+		data = request.form['version']
+	return render_template("./home-page/index.html", version=data)
+
+@app.route('/login')
+def login():
+	return render_template("./login/index.html", version='4')
+
+@app.route('/register')
+def register():
+	return render_template("./register/index.html", version='4')
 
 app.run(host='0.0.0.0', port='8080', debug=True)
